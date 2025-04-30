6d90ea30dfa9426bf9b313b4acb8841fdd64519d
RafaelaSantos123 <rafaelasantosdeveloper@gmail.com>
2025-04-30 10:56:35 -0300

    (30/04) AuthGuard e organização de rotas no front-end

---
+++ front-end/src/routes/routes.jsx
@@ -0,0 +1,81 @@
+// Define as rotas e suas informações, servindo como fonte unica de verdade 
+// para AppRoutes.js e MainMenu.js
+
+import Homepage from '../pages/HomePage'
+
+import LoginPage from '../pages/LoginPage'
+
+import AuthGuard from './AuthGuard'
+
+import CustomersList from '../pages/customers/CustomersList'
+import CustomersForm from '../pages/customers/CustomersForm'
+
+import CarsList from '../pages/cars/CarsList'
+import CarsForm from '../pages/cars/CarsForm'
+
+import UsersList from '../pages/users/UsersList'
+
+const routes = [
+    {
+        route: '/',
+        description: 'Início',
+        element: <Homepage />,
+        userLevel: 0,
+        divider: true
+    },
+    {
+        route: '/login',
+        description: 'Entrar',
+        element: <LoginPage />,
+        userLevel: 0,
+        omitFromMainMenu: true,
+    },
+    {
+        route: '/customers',
+        description: 'Listagem de clientes',
+        element: <CustomersList />,
+        userLevel: 1
+    },
+    {
+        route: '/customers/new',
+        description: 'Cadastro de clientes',
+        element: <CustomersForm />,
+        userLevel: 1,
+        divider: true
+    },
+    {
+        route: '/customers/:id',
+        description: 'Alterar cliente',
+        element: <CustomersForm />,
+        userLevel: 2,
+        omitFromMainMenu: true
+    },
+    {
+        route: '/cars',
+        description: 'Listagem de veículos',
+        element: <CarsList />,
+        userLevel: 1
+    },
+    {
+        route: '/cars/new',
+        description: 'Cadastro de veículos',
+        element: <CarsForm />,
+        userLevel: 1,
+        divider: true
+    },
+    {
+        route: '/cars/:id',
+        description: 'Alterar veículo',
+        element: <carsForm />,
+        userLevel: 2,
+        omitFromMainMenu: true
+    },
+    {
+        route: '/users',
+        description: 'Listagem de usuários',
+        element: <UsersList />,
+        userLevel: 2
+    },
+]
+
+export default routes 
