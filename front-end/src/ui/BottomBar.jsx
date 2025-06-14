b8cd830b93b42c8e5a56059b1980c50f90555315
peida-nao-xerequinha <pedro.tries22@gmail.com>
2025-06-13 22:01:13 -0300

    atualizacoes na bottom bar

--- front-end/src/ui/BottomBar.jsx
+++ front-end/src/ui/BottomBar.jsx
@@ -1,46 +1,41 @@
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
-import CoffeeIcon from '@mui/icons-material/Coffee';
+import LiquorIcon from '@mui/icons-material/Liquor';
 
 export default function BottomBar() {
-  /*sx= estilos estendidos do MUI, estende styles */
   return (
     <Toolbar
       variant="dense"
       sx={{
         backgroundColor: 'action.disabledBackground',
-        justifyContent: 'center',
+        justifyContent: 'center',  // CORRETO: Centraliza o conteúdo
         position: 'fixed',
         bottom: 0,
         width: '100vw'
       }}
     >
       <Typography
-        variant="caption"
+        variant="body1"
         gutterBottom
         sx={{
-          display: 'block',
           '& a': {
             color: 'secondary.light'
           }
-
         }}
       >
         Desenvolvido e mantido com
-        <CoffeeIcon fontSize="small" sx={{ mb: -0.5 }} />
-        {' '}
+        <LiquorIcon fontSize="large" sx={{ mb: -0.5, mx: 0.5 }} /> {}
         por
-        <span style={{ marginLeft: '4px' }} /> {/* Adiciona um espaço extra */}
+        <span style={{ marginLeft: '4px' }} />
         <a href="https://github.com/peida-nao-xerequinha/ps-ads-2025-1">
           peida-nao-xerequinha
         </a>
         <img 
-          src="../../../public/meme.jpg" 
+          src="/meme.jpg" 
           alt="peida-nao-xerecosa" 
-          style={{ width: '25px', height: 'auto', marginLeft: '8px', borderRadius: '8px' }} 
+          style={{ width: '25px', height: 'auto', marginLeft: '8px', borderRadius: '8px', verticalAlign: 'middle' }} 
         />
       </Typography>
     </Toolbar>
   )
-
 }
